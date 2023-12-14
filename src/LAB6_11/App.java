5cd11a698132a2a138b5cdad0a58fe61c5fd8430
lkulig-wsb <lkulig@student.wsb-nlu.edu.pl>
2023-12-14 19:11:21 +0100

    LAB6_11 Commit 1.

---
+++ src/LAB6_11/App.java
@@ -0,0 +1,16 @@
+package LAB6_11;
+
+class Osoba{
+    private String imie;
+    private String nazwisko;
+
+    public Osoba(String imie, String nazwisko){
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+}
+public class App {
+    public static void main(String[] args){
+
+    }
+}
