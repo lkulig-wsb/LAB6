58b2d492bc9c88de83ed209ee83ecf6b5248b73e
lkulig-wsb <lkulig@student.wsb-nlu.edu.pl>
2023-12-14 18:27:01 +0100

    LAB6_9 Commit 2.

--- src/LAB6_9/Pracownik.java
+++ src/LAB6_9/Pracownik.java
@@ -1,25 +1,26 @@
 package LAB6_9;
 
+//Klasa Pracownik.
 public class Pracownik {
+    //Dane składowe klasy pracownik.
     private String imie;
     private String nazwisko;
     private String stanowisko;
 
+    //Konstruktor klasy Pracownik.
     public Pracownik(String imie, String nazwisko, String stanowisko){
         this.imie = imie;
         this.nazwisko = nazwisko;
         this.stanowisko = stanowisko;
     }
-
-
+    //Metoda dispInfo();
+    public void dispInfo(){
+        System.out.println("Imie: " + imie + ", Nazwisko: " + nazwisko + ", Stanowisko: " + stanowisko);
+    }
     public static void main(String[] args)
     {
-        Pracownik pracownik = new Pracownik("Józef", "Cieśla", "Administrator Oracle");
-
-        System.out.println("Imię: " + pracownik.imie);
-        System.out.println("Nazwisko: " + pracownik.nazwisko);
-        System.out.println("Stanowisko: " + pracownik.stanowisko);
-
+         //Utworzenie obiektu anoniomowego. Jednorazowa instancja pracownika z określonymi danymi.
+        //Obiekt anonimowy nie ma przypisanej nazwy. Nie ma deklaracji zmiennej.
+        new Pracownik("Józef", "Cieśla", "Administrator Oracle").dispInfo();
     }
-
 }
