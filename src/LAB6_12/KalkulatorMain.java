1ba139cd73af83461faa50d5b12e4fb5116d10e1
lkulig-wsb <lkulig@student.wsb-nlu.edu.pl>
2023-12-15 15:14:33 +0100

    LAB6_12 Commit 2.

--- src/LAB6_12/KalkulatorMain.java
+++ src/LAB6_12/KalkulatorMain.java
@@ -2,8 +2,55 @@ package LAB6_12;
 
 class Kalkulator{
 
-    public int dodajInt(int a, int b){
+    //Dodawanie int.
+    public int dodaj(int a, int b){
         return a + b;
+    }
+    //Dodawanie double.
+    public double dodaj(double a, double b){
+        return a + b;
+    }
+    //Odejmowanie int.
+    public int odejmij(int a, int b){
+        return a - b;
+    }
+    //Odejmowanie double.
+    public double odejmij(double a, double b){
+        return a - b;
+    }
+    //Mnożenie int.
+    public int pomnoz(int a, int b){
+        return a * b;
+    }
+    //Mnożenie double.
+    public double pomnoz(double a, double b){
+        return a * b;
+    }
+    //Dzielenie int.
+    public int dziel(int a , int b){
+        if(b==0){
+            System.out.println("Error! Nie można dzielić przez 0");
+            return 0;
+        }else {
+            return a/b;
+        }
+    }
+    //Dzielenie double.
+    public double dziel(double a, double b){
+        if(b==0){
+            System.out.println("Error! Nie można dzielić przez 0.");
+            return 0.0;
+        }else {
+            return a/b;
+        }
+    }
+    //Potęga int.
+    public int potega(int pod, int wyk){
+        return (int)Math.pow(pod,wyk);
+    }
+    //Potęga double.
+    public double potega(double pod, double wyk){
+        return Math.pow(pod,wyk);
     }
 
 }
@@ -12,13 +59,21 @@ public class KalkulatorMain {
 
     public static void main(String[] args){
 
+        //Utworzenie obiektu klasy kalkulator do przetestowania przeciążonych metod działań matematycznych.
         Kalkulator k1 = new Kalkulator();
-        System.out.println(k1.dodajInt(5,3));
 
-
+        System.out.println("Dodaj Int: " +k1.dodaj(5,3));
+        System.out.println("Odejmij Int: "+ k1.odejmij(10,4));
+        System.out.println("Dodaj Double:" +k1.dodaj(4.5,13.2));
+        System.out.println("Odejmij Double: " + k1.odejmij(44.6,23.5));
+        System.out.println("Pomnóż Int: " + k1.pomnoz(3,5));
+        System.out.println("Pomnóż Double: " + k1.pomnoz(3.14, 6.54));
+        System.out.println("Podziel Int: " + k1.dziel(100,5));
+        System.out.println("Podziel Double" + k1.dziel(3.14,2.5));
+        //Test dzielenia przez 0.
+        System.out.println( + k1.dziel(20,0));
+        //Potęga.
+        System.out.println("Potęga Int: " + k1.potega(5,3));
+        System.out.println("Potęga Double: " + k1.potega(2.0,4.0));
     }
-
-
-
-
 }
