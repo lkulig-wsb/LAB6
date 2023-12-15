0e317cf384e6e71101fc50d1b15301e07ac1ae4e
lkulig-wsb <lkulig@student.wsb-nlu.edu.pl>
2023-12-15 13:53:29 +0100

    LAB6_12 Initial Commit.

---
+++ src/LAB6_12/KalkulatorMain.java
@@ -0,0 +1,24 @@
+package LAB6_12;
+
+class Kalkulator{
+
+    public int dodajInt(int a, int b){
+        return a + b;
+    }
+
+}
+
+public class KalkulatorMain {
+
+    public static void main(String[] args){
+
+        Kalkulator k1 = new Kalkulator();
+        System.out.println(k1.dodajInt(5,3));
+
+
+    }
+
+
+
+
+}
